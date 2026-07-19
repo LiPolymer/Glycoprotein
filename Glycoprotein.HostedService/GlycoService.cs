7d21effcbb67c0d56f89e8d153fab9ef3e7e3e18
LiPolymer <fish@lipoly.ink>
2026-07-19 23:58:54 +0800

    proj: add Glycoprotein.HostedService

---
+++ Glycoprotein.HostedService/GlycoService.cs
@@ -0,0 +1,11 @@
+﻿using Glycoprotein.Connexon;
+using Microsoft.Extensions.Hosting;
+
+namespace Glycoprotein.HostedService;
+
+public class GlycoService(string id,IConnexon? connexon = null) : GlycoComplex(id,connexon),IHostedService {
+    public Task StopAsync(CancellationToken cancellationToken) {
+        Dispose();
+        return Task.CompletedTask;
+    }
+}
